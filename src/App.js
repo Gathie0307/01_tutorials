c641af8656ee80c86588a669c0e06a934f583f76
Gayathri Krishnan <iamkgayu@gmail.com>
2025-03-17 15:13:26 +0000

    Added three new Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Content from './Content';
+import Header from './Header';
+import Footer from './Footer';
 
 function App() {
-
-  const handleNameChange =  () => {
-    const names = ["Gayathri", "Ragupathi", "Yara"];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
