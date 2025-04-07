4739f42962d7d821592e634c7cd675c66deb6d71
Gayathri Krishnan <iamkgayu@gmail.com>
2025-04-07 23:13:42 +0100

    Updated the Todo app with update feature

--- src/App.js
+++ src/App.js
@@ -7,24 +7,8 @@ import {useState} from 'react';
 import AddItem from './AddItem';
 
 function App() {
-  const[items, setItems] = useState(
-      [
-        {
-            id: 1,
-            checked: true,
-            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-        },
-        {
-            id: 2,
-            checked: false,
-            item: "Item 2"
-        },
-        {
-            id: 3,
-            checked: false,
-            item: "Item 3"
-        }
-    ]);
+  const[items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist'))
+    );
     const[newItem, setNewItem] = useState('')
 
     const handleCheck = (id) => {
@@ -32,20 +16,33 @@ function App() {
       const listItems = items.map((item) =>
         item.id === id ? {...item, checked : !item.checked} : item
       );
-      setItems(listItems)
-      localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+      setAndSaveItem(listItems);
     }
 
     const handleDelete = (id) => {
       const listItems = items.filter((item) => item.id !== id);
-      setItems(listItems);
-      localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+      setAndSaveItem(listItems);
     }
 
-    const handleSubmit = (e) => {
-      console.log(e)
+    const setAndSaveItem = (newItems) => {
+      setItems(newItems)
+      localStorage.setItem('shoppinglist', JSON.stringify(newItems));
     }
-    
+
+    const addItem = (item) => {
+      const id = items.length ? items[items.length - 1].id + 1 : 1;
+      const myNewItem = {id, checked: true, item}
+      const listItems = [...items, myNewItem];
+      setAndSaveItem(listItems);
+    }
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if(!newItem) return;
+      addItem(newItem);
+      setNewItem('')
+
+    }
+
   return (
     <div className='App'>
       <Header title="Grocery List"/>
