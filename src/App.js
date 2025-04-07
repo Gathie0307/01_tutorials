785a98fb29c72f9cf4d14d0552c4fb87e9973aa5
Gayathri Krishnan <iamkgayu@gmail.com>
2025-04-07 10:47:15 +0100

    Added Input form to Add Grocery List

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Content from './Content';
 import Header from './Header';
 import Footer from './Footer';
 import {useState} from 'react';
+import AddItem from './AddItem';
 
 function App() {
   const[items, setItems] = useState(
@@ -24,6 +25,7 @@ function App() {
             item: "Item 3"
         }
     ]);
+    const[newItem, setNewItem] = useState('')
 
     const handleCheck = (id) => {
       console.log(`key: ${id}`);
@@ -40,9 +42,14 @@ function App() {
       localStorage.setItem('shoppinglist', JSON.stringify(listItems));
     }
 
+    const handleSubmit = (e) => {
+      console.log(e)
+    }
+    
   return (
     <div className='App'>
       <Header title="Grocery List"/>
+      <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
       <Content items={items} handleCheck={handleCheck} handleDelete={handleDelete} />
       <Footer length={items.length}/>
     </div>
