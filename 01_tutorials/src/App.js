e33c5e267573f6d4ee4ed3bc54c3b9bf3cecee42
Gayathri Krishnan <iamkgayu@gmail.com>
2025-04-24 10:38:16 +0100

    Completed Crud opertions to update the data.json

--- 01_tutorials/src/App.js
+++ 01_tutorials/src/App.js
@@ -40,20 +40,42 @@ function App() {
 
      }, []);
 
-    const handleCheck = (id) => {
+    const handleCheck = async(id) => {
       console.log(`key: ${id}`);
       const listItems = items.map((item) =>
         item.id === id ? {...item, checked : !item.checked} : item
       );
       setItems(listItems);
+
+      const myItem = listItems.filter((item) => item.id === id)
+
+      const updateOptions = {
+        method: 'PATCH',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({checked: myItem[0].checked})
+      }
+
+      const reqUrl = `${API_URL}/${id}`;
+      const result = await apiRequest(reqUrl, updateOptions);
+      if(result) setFetchError(result);
     }
 
-    const handleDelete = (id) => {
+    const handleDelete = async(id) => {
       const listItems = items.filter((item) => item.id !== id);
       setItems(listItems);
+
+      const deleteOptions = {
+        method: 'DELETE'
+      }
+
+      const reqUrl = `${API_URL}/${id}`;
+      const result = await apiRequest(reqUrl, deleteOptions);
+      if(result) setFetchError(result);
     }
 
-    const addItem = (item) => {
+    const addItem = async(item) => {
       const id = items.length ? items[items.length - 1].id + 1 : 1;
       const myNewItem = {id, checked: false, item}
       const listItems = [...items, myNewItem];
@@ -69,6 +91,7 @@ function App() {
 
       const result = await apiRequest(API_URL, postOptions);
       if(result) setFetchError(result);
+
     }
     const handleSubmit = (e) => {
       e.preventDefault();
